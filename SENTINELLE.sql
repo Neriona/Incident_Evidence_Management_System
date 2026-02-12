7e7247fcbc5a2ae663a29069e0355aeeb1cfc7b8
Haytham Kennouz <kennouzhaytham@gmail.com>
2026-02-12 11:30:20 +0100

    Clean up comments and describe PL/SQL project
    Removed project header comments and added a brief description of the PL/SQL project.

--- SENTINELLE.sql
+++ SENTINELLE.sql
@@ -1,25 +1,3 @@
-/* ============================================================
-   Project Name : SENTINELLE
-   Description  : Système de gestion des incidents et des preuves numériques
-                  (Incident & Digital Evidence Management System)
-
-   Technologies : Oracle Database, PL/SQL
-   Domain       : Cybersecurity / Digital Forensics
-
-   Developed by :
-     - Rayhana Laznasni
-     - Mohammed Khamrich
-     - Othmane Laarich
-
-   Collaboration : Team Project – Joint Development
-   Version       : 1.0
-   ============================================================ */
-
-# Incident_Evidence_Management_System
---PL/SQL project designed to collect evidence and incident information related to specific types of attacks, with the ability to add and modify them.
--- =========================
--- CLEANING: Drop tables safely
--- =========================
 BEGIN
     EXECUTE IMMEDIATE 'DROP TABLE ACTION_LOG CASCADE CONSTRAINTS';
     EXECUTE IMMEDIATE 'DROP TABLE EVIDENCE CASCADE CONSTRAINTS';
